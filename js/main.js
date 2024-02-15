5c704d2cad07515d5f36aee8256ecd8b4cf28fc6
amsornet <amsornet@google.com>
2024-02-15 16:28:45 +0300

    init JS

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// console.log("Hellow");
+//Бургер
+
+const burger = document.getElementById("burger");
+const nav = document.getElementById("nav");
+
+burger.addEventListener("click", function(){
+  nav.classList.toggle("nav--active");
+  burger.classList.toggle("burger--active");
+  document.body.classList.toggle("stop-scroll");
+
+});
+
+
