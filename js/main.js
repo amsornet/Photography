26684a123e88b3ece16cc63b73fe689e7340b58e
amsornet <amsornet@gmail.com>
2024-02-17 23:05:48 +0300

    init js modalForm close / open onclick

--- js/main.js
+++ js/main.js
@@ -11,4 +11,36 @@ burger.addEventListener("click", function(){
 
 });
 
+//modal
+const callFormBtn = document.getElementById("call-form");
+const modalCallForm = document.getElementById("modal-call-form");
+
+//open modal
+callFormBtn.addEventListener("click", function(){
+  modalCallForm.classList.add("modal-parent--open");
+});
+
+//close modal
+
+modalCallForm.querySelector(".modal").addEventListener("click", function(event){
+    event._isClick = true;
+});
+
+modalCallForm.addEventListener("click", function(event){
+  if (event._isClick === true) return
+  modalCallForm.classList.remove("modal-parent--open");
+});
+
+//close form click ESC
+
+window.addEventListener("keydown", function(event){
+  if(event.key === "Escape") {
+    modalCallForm.classList.remove("modal-parent--open");
+  }
+})
+
+
+
+
+
 
